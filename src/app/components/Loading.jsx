566f2f51a62e0fef677913af6e66acde9c0c7eaf
biswass101 <biswassnaeemcse@gmail.com>
2025-03-05 05:55:30 +0600

    component updated

--- src/app/components/Loading.jsx
+++ src/app/components/Loading.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 export default function Loading() {
   return (
     <div className='w-full h-screen font-bold text-2xl md:text-4xl flex justify-center items-center'>
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import Cart from '../components/Cart'
 import { useRouter } from 'next/navigation'
